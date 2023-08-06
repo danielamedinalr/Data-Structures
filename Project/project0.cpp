2cd88389d6cbe828334780a81f8db9088a98fe71
Camachik <140348895+Camachik@users.noreply.github.com>
2023-08-06 10:19:57 -0500

    split reading inputs to its own function, changed commands to a vector of strings

--- Project/project0.cpp
+++ Project/project0.cpp
@@ -1,12 +1,42 @@
 #include<iostream>
 #include<cstdlib>
 #include<string>
+#include<sstream>
 #include<vector>
 
 using namespace std;
 
-void confighelp()
+vector <string> readinputs()
 {
+    string consoleinput, line, option;
+    
+    vector <string> commands;
+
+    //sometimes getline misbehaves because of /n
+    //so sometimes we have to use cin.ignore()
+    
+    getline(cin, consoleinput);
+    stringstream inputs(consoleinput);
+
+    while(inputs >> line)
+    {
+        commands.push_back(line);
+    }
+    
+    for(string &cmds: commands)
+    {
+        for (int q = 0; q < cmds.length(); q++)
+        {
+            cmds[q] = tolower(cmds[q]);
+        }
+    }
+
+    return commands;
+}
+
+void confighelp(vector <string> &helpline)
+{
+    string option;
     vector <vector <string> > confighelp;
     confighelp.push_back({"help", "shows available commands and their descriptions, depending on the state of the program"});
     confighelp.push_back({"initialize", "asks number of players, the name of each player, and asks which territories each player wants to occupy in their turn"});
@@ -14,6 +44,28 @@ void confighelp()
     confighelp.push_back({"clear", "resets the screen of the terminal"});
     confighelp.push_back({"exit", "ends the program"});
 
+    if (helpline.size() == 1)
+    {
+        cout << endl << "available commands: " << endl << endl;
+        for(int q = 0; q < confighelp.size(); q++)
+        {
+            cout << confighelp[q][0] << endl;
+        }
+        cout << endl << "type help -command to get a description of each command" << endl << endl;
+    }
+    else
+    {
+        option = helpline[1].substr(1);
+        for (int q = 0; q < confighelp.size(); q++)
+        {
+            if (confighelp[q][0] == option)
+            {
+                cout << endl << confighelp[q][0] << ": " << confighelp[q][1] << endl << endl;
+                break;
+            }
+        }
+    }
+    
     /*
     //range-based loops
     for (vector <string> &loopvec : helpvector)
@@ -26,7 +78,8 @@ void confighelp()
         cout << endl;
     }
     */
-
+    /*
+    //normal loops
     for (int q = 0; q < confighelp.size(); q++)
     {
         for (int w = 0; w < confighelp[q].size(); w++)
@@ -35,25 +88,23 @@ void confighelp()
         }
         cout << endl << endl;
     }
+    */
+
 }
 
 void gameconfig(bool &initstate)
 {
     //Componente 1: configuracion del juego
     bool endgame = false, validplayer = false, playerturn = false, endturn = false;
-    string command;
+    vector <string> commands;
     
     do{
-        cin >> command;
-        for(int q = 0; q < command.length(); q++)
-        {
-            command[q] = tolower(command[q]); 
+        commands = readinputs();
+        
+        if (commands[0] == "help"){
+            confighelp(commands);
         }
-
-        if (command == "help"){
-            confighelp();
-        }
-        else if (command == "initialize")
+        else if (commands[0] == "initialize")
         {
             if(initstate == true)
             {
@@ -65,7 +116,7 @@ void gameconfig(bool &initstate)
                 cout << "game initialized succesfully" << endl;
             }
         }
-        else if (command == "turn")
+        else if (commands[0] == "turn")
         {
             if (initstate == false)
             {
@@ -88,11 +139,11 @@ void gameconfig(bool &initstate)
                 cout << "turn of current player has ended" << endl;
             }
         }
-        else if (command == "exit")
+        else if (commands[0] == "exit")
         {
             break;
         }
-        else if (command == "clear")
+        else if (commands[0] == "clear")
         {
             #ifdef _WIN32
                 system("cls");
@@ -104,13 +155,90 @@ void gameconfig(bool &initstate)
         {
             cout << "invalid command, type help to get a list of available commands" << endl;
         }
-    } while (command != "exit");
+    } while (commands[0] != "exit");
+}
+
+void gamesaves(bool &gameinit)
+{
+    bool savefine = false, readfine = false;
+    vector <string> commands;
+
+    do
+    {
+        commands = readinputs();
+
+        if (commands[0] == "help")
+        {
+            
+        }
+        else if (commands[0] == "save")
+        {
+            if(gameinit == false)
+            {
+                cout << "game has not been initialized correctly" << endl;
+            }
+            else if (savefine == false)
+            {
+                cout << "save error, game has not been saved correctly" << endl;
+            }
+            else
+            {
+                cout << "game saved correctly" << endl;
+            }
+        }
+        else if (commands[0] == "save_compressed")
+        {
+            if(gameinit == false)
+            {
+                cout << "game has not been initialized correctly" << endl;
+            }
+            else if (savefine == false)
+            {
+                cout << "save and compression error, game has not been saved nor compressed correctly" << endl;
+            }
+            else
+            {
+                cout << "game saved and compressed correctly" << endl;
+            }
+        }
+        else if (commands[0] == "initialize")
+        {
+            if(readfine == false)
+            {
+                if(commands.size() == 1)
+                {
+                    cout << "no file name specified" << endl;
+                }
+                else
+                {
+                    cout << commands[1] << " does not contain valid info to initialize the game" << endl;
+                }
+            }
+            else
+            {
+                gameinit = true;
+                cout << "game initialized from file correctly" << endl;
+            }
+        }
+        else if (commands[0] == "clear")
+        {
+            #ifdef _WIN32
+                system("cls");
+            #else
+                system("clear");
+            #endif
+        } 
+        else
+        {
+            cout << "invalid command, type help to get a list of available commands" << endl;
+        }
+    } while (commands[0] != "exit");
 }
 
 int main()
 {
    bool gameinit = false;
-   
+
    gameconfig(gameinit);
     
 }
